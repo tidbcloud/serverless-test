45255ed8d4c0bafa18e570cf4f4d4771d2e57f5a
shiyuhang <1136742008@qq.com>
2024-10-21 16:16:09 +0800

    add config

--- config/config.go
+++ config/config.go
@@ -141,6 +141,7 @@ func getDefault() {
 func getConfig() {
 	viper.SetConfigType("toml")
 	if configContent != "" {
+		println(configContent)
 		err := viper.ReadConfig(bytes.NewBuffer([]byte(configContent)))
 		if err != nil {
 			println("Error reading config: ", err.Error())
