1e78c1e98ca562ce7b4dc97181b408b96195883b
yangxin <yangxin@pingcap.com>
2025-07-03 10:37:31 +0800

    Add customMapping test cases

--- sceneTest/webImport/precheck_test.go
+++ sceneTest/webImport/precheck_test.go
@@ -174,55 +174,164 @@ func TestPrecheckTruncatedResult(t *testing.T) {
 	util.EqualPointerValues(assert, pointer.ToString("997"), result.TotalDataFilesCount)
 }
 
-// TODO - Uncomment and implement this test when the precheck logic for a single file is ready.
-//func TestPrecheckOneFile(t *testing.T) {
-//	ctx := context.Background()
-//	assert := require.New(t)
-//	_, err := db.Exec("DROP TABLE IF EXISTS `test`.`a`")
-//	assert.NoError(err)
-//	r := importClient.ImportServiceAPI.ImportServicePrecheck(ctx, orgId, projectId, clusterId)
-//	r = r.Body(consoleimportapi.ImportServicePrecheckBody{
-//		ImportOptions: consoleimportapi.ImportOptions{
-//			FileType: consoleimportapi.IMPORTFILETYPEENUM_CSV,
-//		},
-//		Source: consoleimportapi.ImportSource{
-//			Type: consoleimportapi.IMPORTSOURCETYPEENUM_S3,
-//			S3: &consoleimportapi.S3Source{
-//				Uri:      config.ImportS3URI,
-//				AuthType: consoleimportapi.IMPORTS3AUTHTYPEENUM_ROLE_ARN,
-//				RoleArn:  &config.ImportS3RoleArn,
-//				TargetTableInfos: []consoleimportapi.ImportTargetTableInfo{
-//					{
-//						TargetTable: &consoleimportapi.ConsoleTable{
-//							Database: pointer.ToString("test"),
-//							Table:    pointer.ToString("a"),
-//						},
-//						CustomFile: pointer.ToString("test.a.csv"),
-//					},
-//				},
-//			},
-//		},
-//	})
-//	result, resp, err := r.Execute()
-//	err = util.ParseError(err, resp)
-//
-//	assert.NoError(err)
-//	assert.NotNil(result)
-//	util.EqualPointerValues(assert, pointer.ToBool(false), result.IsTruncated)
-//	util.EqualPointerValues(assert, pointer.ToString("Found 1 table(s) with error: test.a"), result.ErrorMessage)
-//	util.EqualPointerValues(assert, pointer.ToString("1"), result.TotalTablesCount)
-//	util.EqualPointerValues(assert, pointer.ToString("1"), result.TotalDataFilesCount)
-//	// Verify table result
-//	assert.ElementsMatch([]consoleimportapi.TableResult{
-//		{
-//			TargetTable: &consoleimportapi.ConsoleTable{
-//				Database: pointer.ToString("test"),
-//				Table:    pointer.ToString("a"),
-//			},
-//			ErrorMessage:      pointer.ToString("table test.a is not empty"),
-//			MatchedDataFiles:  []string{"test.a.csv"},
-//			MatchedSchemaFile: pointer.ToString("test.a-schema.sql.gz"),
-//			UseSchemaFile:     pointer.ToBool(false),
-//		},
-//	}, result.TargetTables)
-//}
+func TestPrecheckCustomMappingWithoutTable(t *testing.T) {
+	ctx := context.Background()
+	assert := require.New(t)
+	_, err := db.Exec("DROP TABLE IF EXISTS `test`.`a`")
+	assert.NoError(err)
+	r := importClient.ImportServiceAPI.ImportServicePrecheck(ctx, orgId, projectId, clusterId)
+	r = r.Body(consoleimportapi.ImportServicePrecheckBody{
+		ImportOptions: consoleimportapi.ImportOptions{
+			FileType: consoleimportapi.IMPORTFILETYPEENUM_CSV,
+		},
+		Source: consoleimportapi.ImportSource{
+			Type: consoleimportapi.IMPORTSOURCETYPEENUM_S3,
+			S3: &consoleimportapi.S3Source{
+				Uri:      config.ImportS3URI,
+				AuthType: consoleimportapi.IMPORTS3AUTHTYPEENUM_ROLE_ARN,
+				RoleArn:  &config.ImportS3RoleArn,
+				TargetTableInfos: []consoleimportapi.ImportTargetTableInfo{
+					{
+						TargetTable: &consoleimportapi.ConsoleTable{
+							Database: pointer.ToString("test"),
+							Table:    pointer.ToString("a"),
+						},
+						CustomFile: pointer.ToString("test.a.csv"),
+					},
+				},
+			},
+		},
+	})
+	result, resp, err := r.Execute()
+	err = util.ParseError(err, resp)
+
+	assert.NoError(err)
+	assert.NotNil(result)
+	util.EqualPointerValues(assert, pointer.ToBool(false), result.IsTruncated)
+	util.EqualPointerValues(assert, pointer.ToString("Found 1 table(s) with error: test.a"), result.ErrorMessage)
+	util.EqualPointerValues(assert, pointer.ToString("1"), result.TotalTablesCount)
+	util.EqualPointerValues(assert, pointer.ToString("1"), result.TotalDataFilesCount)
+	// Verify table result
+	assert.ElementsMatch([]consoleimportapi.TableResult{
+		{
+			TargetTable: &consoleimportapi.ConsoleTable{
+				Database: pointer.ToString("test"),
+				Table:    pointer.ToString("a"),
+			},
+			ErrorMessage:      pointer.ToString("table test.a does not exist"),
+			MatchedDataFiles:  []string{"test.a.csv"},
+			MatchedSchemaFile: pointer.ToString(""),
+			UseSchemaFile:     pointer.ToBool(false),
+		},
+	}, result.TargetTables)
+}
+
+func TestPrecheckCustomMappingWithNonEmptyTable(t *testing.T) {
+	ctx := context.Background()
+	assert := require.New(t)
+	_, err := db.Exec("DROP TABLE IF EXISTS `test`.`a`")
+	assert.NoError(err)
+	_, err = db.Exec("CREATE TABLE `test`.`a` (name VARCHAR(20) NOT NULL, age INT NOT NULL)")
+	assert.NoError(err)
+	_, err = db.Exec("INSERT INTO `test`.`a` VALUES ('Alice', 30), ('Bob', 25)")
+	assert.NoError(err)
+	r := importClient.ImportServiceAPI.ImportServicePrecheck(ctx, orgId, projectId, clusterId)
+	r = r.Body(consoleimportapi.ImportServicePrecheckBody{
+		ImportOptions: consoleimportapi.ImportOptions{
+			FileType: consoleimportapi.IMPORTFILETYPEENUM_CSV,
+		},
+		Source: consoleimportapi.ImportSource{
+			Type: consoleimportapi.IMPORTSOURCETYPEENUM_S3,
+			S3: &consoleimportapi.S3Source{
+				Uri:      config.ImportS3URI,
+				AuthType: consoleimportapi.IMPORTS3AUTHTYPEENUM_ROLE_ARN,
+				RoleArn:  &config.ImportS3RoleArn,
+				TargetTableInfos: []consoleimportapi.ImportTargetTableInfo{
+					{
+						TargetTable: &consoleimportapi.ConsoleTable{
+							Database: pointer.ToString("test"),
+							Table:    pointer.ToString("a"),
+						},
+						CustomFile: pointer.ToString("test.a.csv"),
+					},
+				},
+			},
+		},
+	})
+	result, resp, err := r.Execute()
+	err = util.ParseError(err, resp)
+
+	assert.NoError(err)
+	assert.NotNil(result)
+	util.EqualPointerValues(assert, pointer.ToBool(false), result.IsTruncated)
+	util.EqualPointerValues(assert, pointer.ToString(""), result.ErrorMessage)
+	util.EqualPointerValues(assert, pointer.ToString("1"), result.TotalTablesCount)
+	util.EqualPointerValues(assert, pointer.ToString("1"), result.TotalDataFilesCount)
+	// Verify table result
+	assert.ElementsMatch([]consoleimportapi.TableResult{
+		{
+			TargetTable: &consoleimportapi.ConsoleTable{
+				Database: pointer.ToString("test"),
+				Table:    pointer.ToString("a"),
+			},
+			ErrorMessage:      pointer.ToString("table test.a is not empty"),
+			MatchedDataFiles:  []string{"test.a.csv"},
+			MatchedSchemaFile: pointer.ToString(""),
+			UseSchemaFile:     pointer.ToBool(false),
+		},
+	}, result.TargetTables)
+}
+
+func TestPrecheckCustomMappingWithEmptyTable(t *testing.T) {
+	ctx := context.Background()
+	assert := require.New(t)
+	_, err := db.Exec("DROP TABLE IF EXISTS `test`.`a`")
+	assert.NoError(err)
+	_, err = db.Exec("CREATE TABLE `test`.`a` (name VARCHAR(20) NOT NULL, age INT NOT NULL)")
+	assert.NoError(err)
+	r := importClient.ImportServiceAPI.ImportServicePrecheck(ctx, orgId, projectId, clusterId)
+	r = r.Body(consoleimportapi.ImportServicePrecheckBody{
+		ImportOptions: consoleimportapi.ImportOptions{
+			FileType: consoleimportapi.IMPORTFILETYPEENUM_CSV,
+		},
+		Source: consoleimportapi.ImportSource{
+			Type: consoleimportapi.IMPORTSOURCETYPEENUM_S3,
+			S3: &consoleimportapi.S3Source{
+				Uri:      config.ImportS3URI,
+				AuthType: consoleimportapi.IMPORTS3AUTHTYPEENUM_ROLE_ARN,
+				RoleArn:  &config.ImportS3RoleArn,
+				TargetTableInfos: []consoleimportapi.ImportTargetTableInfo{
+					{
+						TargetTable: &consoleimportapi.ConsoleTable{
+							Database: pointer.ToString("test"),
+							Table:    pointer.ToString("a"),
+						},
+						CustomFile: pointer.ToString("test.a.csv"),
+					},
+				},
+			},
+		},
+	})
+	result, resp, err := r.Execute()
+	err = util.ParseError(err, resp)
+
+	assert.NoError(err)
+	assert.NotNil(result)
+	util.EqualPointerValues(assert, pointer.ToBool(false), result.IsTruncated)
+	util.EqualPointerValues(assert, pointer.ToString(""), result.ErrorMessage)
+	util.EqualPointerValues(assert, pointer.ToString("1"), result.TotalTablesCount)
+	util.EqualPointerValues(assert, pointer.ToString("1"), result.TotalDataFilesCount)
+	// Verify table result
+	assert.ElementsMatch([]consoleimportapi.TableResult{
+		{
+			TargetTable: &consoleimportapi.ConsoleTable{
+				Database: pointer.ToString("test"),
+				Table:    pointer.ToString("a"),
+			},
+			ErrorMessage:      pointer.ToString(""),
+			MatchedDataFiles:  []string{"test.a.csv"},
+			MatchedSchemaFile: pointer.ToString(""),
+			UseSchemaFile:     pointer.ToBool(false),
+		},
+	}, result.TargetTables)
+}
