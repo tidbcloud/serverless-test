57a357352beac11b268947f8611d60df14959270
shiyuhang <1136742008@qq.com>
2025-10-20 18:47:03 +0800

    add cdc test

--- sceneTest/cdc/cdc_sync_test.go
+++ sceneTest/cdc/cdc_sync_test.go
@@ -13,9 +13,12 @@ import (
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/tidbcloud/serverless-test/config"
+	"github.com/tidbcloud/serverless-test/util"
+	"github.com/tidbcloud/tidbcloud-cli/pkg/tidbcloud/v1beta1/serverless/cdc"
 )
 
 func TestMain(m *testing.M) {
+	setup()
 	code := m.Run()
 	os.Exit(code)
 }
@@ -25,14 +28,21 @@ func TestMySQLSync(t *testing.T) {
 	ctx := context.Background()
 
 	cfg := config.LoadConfig().Changefeed.MySQL
-	t.Log(fmt.Sprintf("start to test mysql changefeed sync, changefeed_id: %s, cluster_id:%s, region:%s",
+	t.Logf("start to test mysql changefeed sync, changefeed_id: %s, cluster_id:%s, region:%s",
 		cfg.ChangefeedID,
 		cfg.ClusterID,
-		cfg.Region))
+		cfg.Region)
+	cf, err := getChangefeed(ctx, cfg.ClusterID, cfg.ChangefeedID)
+	if err != nil {
+		t.Fatalf("failed to get changefeed: %v", err)
+	}
+	if *cf.State != cdc.CHANGEFEEDSTATEENUM_RUNNING {
+		t.Fatalf("changefeed is not running, current state: %s", *cf.State)
+	}
 
 	ts := time.Now().UnixMilli()
 	t.Log("start to insert into upstream tidb cloud cluster")
-	err := executeDB(ctx, cfg.ClusterDSN, fmt.Sprintf("insert into test.cdc (id, name) values (%d, 'cdc')", ts))
+	err = executeDB(ctx, cfg.ClusterDSN, fmt.Sprintf("insert into test.cdc (id, name) values (%d, 'cdc')", ts))
 	if err != nil {
 		t.Fatalf("failed to insert into upstream tidb cloud cluster: %v", err)
 	}
@@ -141,4 +151,10 @@ func queryDB(ctx context.Context, dsn string, query string) (exist bool, err err
 		return true, nil
 	}
 	return false, nil
+}
+
+func getChangefeed(ctx context.Context, clusterId, changefeedId string) (*cdc.Changefeed, error) {
+	r := cdcClient.ChangefeedServiceAPI.ChangefeedServiceGetChangefeed(ctx, clusterId, changefeedId)
+	res, h, err := r.Execute()
+	return res, util.ParseError(err, h)
 }
--- sceneTest/cdc/init_test.go
+++ sceneTest/cdc/init_test.go
@@ -1,10 +1,36 @@
 package cdc
 
-import "github.com/tidbcloud/serverless-test/config"
+import (
+	"log"
+	"net/http"
 
-var clusterId string
+	"github.com/tidbcloud/serverless-test/config"
+	"github.com/tidbcloud/serverless-test/util"
+	"github.com/tidbcloud/tidbcloud-cli/pkg/tidbcloud/v1beta1/serverless/cdc"
+)
 
-func init() {
-	cfg := config.LoadConfig()
-	println(cfg.Changefeed)
+var cdcClient *cdc.APIClient
+
+func setup() {
+	var err error
+	cdcClient, err = NewCDCClient(config.LoadConfig())
+	if err != nil {
+		log.Panicf("failed to create cdc client: %v", err)
+	}
+}
+
+// NewExportClient creates a new export API client with the given configuration
+func NewCDCClient(cfg *config.Config) (*cdc.APIClient, error) {
+	httpClient := &http.Client{
+		Transport: util.NewDigestTransport(cfg.PublicKey, cfg.PrivateKey),
+	}
+	serverlessURL, err := util.ValidateApiUrl(cfg.Endpoint.Serverless)
+	if err != nil {
+		return nil, err
+	}
+	cdcCfg := cdc.NewConfiguration()
+	cdcCfg.HTTPClient = httpClient
+	cdcCfg.Host = serverlessURL.Host
+	cdcCfg.UserAgent = util.UserAgent
+	return cdc.NewAPIClient(cdcCfg), nil
 }
